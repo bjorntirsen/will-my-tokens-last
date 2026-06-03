ad02eb679f70ddd20c8a3b1f64a83f284155ed93
Björn Tirsén <bjorntirsen@gmail.com>
2026-06-03 19:25:48 +0200

    Add cleanup to cache and simplify sentry

--- src/instrument.ts
+++ src/instrument.ts
@@ -3,6 +3,5 @@ import * as Sentry from "@sentry/react";
 if (import.meta.env.PROD) {
   Sentry.init({
     dsn: "https://0708d6cde184dd4181d0483ba87744b5@o4511077606293504.ingest.de.sentry.io/4511151796125776",
-    sendDefaultPii: true,
   });
 }
